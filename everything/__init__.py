c4b6b5c3a5d2dfced9d5bab3e6589b15fc6cb659
Nikola Bebic <nikola.bebic99@gmail.com>
2019-05-26 12:41:57 +0200

    Initial commit

---
+++ everything/__init__.py
@@ -0,0 +1,42 @@
+
+class module:
+    def __init__(self, mod):
+        self.__mod = mod
+        self.__import = None
+
+    def __getattr__(self, name):
+        if not self.__import:
+            self.__import = __import__(self.__mod)
+
+        if hasattr(self.__import, name):
+            return getattr(self.__import, name)
+        elif hasattr(self.__import, '__path__'):
+            return getattr(__import__(self.__mod + '.' + name), name)
+
+    def __str__(self):
+        return self.__getattr__('__str__')()
+
+    def __repr__(self):
+        return self.__getattr__('__repr__')()
+
+def __getattr__(name):
+    return module(name)
+
+import sys
+import os
+from os.path import join, isdir, isfile
+
+__all__ = []
+__path__ = None
+
+for p in sys.path:
+    p = p or '.'
+    if not isdir(p): 
+        continue
+    for l in os.listdir(p):
+        if l.startswith('_'): 
+            continue
+        if (isfile(join(p, l)) and l.endswith('.py')):
+            __all__.append(l[:-3])
+        if (isdir(join(p, l)) and isfile(join(p, l, '__init__.py'))):
+            __all__.append(l)
---
+++ test.py
@@ -0,0 +1,6 @@
+from everything import *
+
+a = numpy.linspace(1,3)
+b = a ** 2
+matplotlib.pyplot.plot(a, b)
+matplotlib.pyplot.show()
