89cefac0713d8aed4ee2ffcc908b424603faca12
Nikola Bebic <nikola.bebic99@gmail.com>
2019-10-02 13:00:43 +0200

    publishing to pypi

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="everything",
+    name="import-everything",
     version="0.0.1",
     author="Nikola Bebić",
     author_email="nikola.bebic99@gmail.com",
